abb483d0093a99c04a65e2e682be1ef90e5bc879
Frederik Scheiderbauer <frederik.scheiderbauer@gmx.de>
2025-08-27 22:42:28 +0200

    Add project files.

---
+++ DirectXGEPTutorial/DirectXGEPTutorial.cpp
@@ -0,0 +1,12 @@
+﻿// DirectXGEPTutorial.cpp : Defines the entry point for the application.
+//
+
+#include "DirectXGEPTutorial.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
